99fa6cbfe074921d0b903dd166ce1d67c2a3860f
Angkhana Chuchuaypan <39267875+5610110639@users.noreply.github.com>
2019-02-26 22:45:55 +0700

    Ex01

---
+++ EX01/Counter.java
@@ -0,0 +1,13 @@
+public class Counter{
+    private int val;
+
+    public void inc(){
+        val++;
+    }
+    public void reset(){
+        val=0;
+    }
+    public int getVal(){
+        return val;
+    }
+}

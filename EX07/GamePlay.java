839e1bd0fd23ebd16a84bcd789bece39ab82d585
Angkhana Chuchuaypan <39267875+5610110639@users.noreply.github.com>
2019-05-13 11:13:04 +0700

    EX07

---
+++ EX07/GamePlay.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+    public class GamePlay{
+    
+     public static void main(String [] args){
+        Game g = new Game(30);
+            g.addTunnel(5,8);
+            g.addTunnel(12,15);
+            g.addTunnel(17,1);
+            g.addTunnel(28,16);
+    while (!g.isEnded()){
+        g.play();
+    }
+    }
+}
